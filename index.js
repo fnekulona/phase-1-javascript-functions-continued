584f6674a58ae79f2afb093e8143cc783fb56b8a
Folasade Ekulona <ekulonaf@gmail.com>
2023-04-09 19:27:30 -0400

    adds completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(toDo = "go to the office"){
+    return `This Monday, I will ${toDo}.`;
+}
+function wrapAdjective(flare = "*"){
+    return function(adjective = "special" ){
+        return `You are ${flare}${adjective}${flare}!`;
+    }
+}
